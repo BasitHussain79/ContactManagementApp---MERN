8f03f3270903d9acecc19940f3d1ffbd82a7e599
unknown <basit.hussain@caary.com>
2023-07-06 20:33:46 +0500

    feat: working on sign up api

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,60 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+
 const router = express.Router();
+
+const User = require("../models/User");
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  public
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Please enter your name").not().isEmpty(),
+    check("email", "Please enter valid email").isEmail(),
+    check(
+      "password",
+      "Please enter password with at least 6 characters"
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // res.send("passed without errors");
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ msg: "A User with this email already exists." });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.status(200).send("User created successfully!");
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
